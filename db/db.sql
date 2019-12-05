e1d4103b09a75dca4c65544de389bdf60e8b5a9d
chenjiefei <nlpx_dc@hotmail.com>
2019-12-05 12:02:33 +0800

    draft sql files

---
+++ db/db.sql
@@ -0,0 +1,49 @@
+SET
+  FOREIGN_KEY_CHECKS = 0;
+--------------------------------------------------------------------------
+  DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+    `student_id` int(11) NOT NULL auto_increment,
+    `nickname` varchar(100),
+    `realname` varchar(20),
+    `openid` varchar(100) not null,
+    `avatar_url` varchar(300),
+    `status` tinyint not null,
+    `gender` tinyint,
+    `mobile` varchar(20),
+    `email` varchar(100),
+    `mobile_verified` bit not null,
+    `email_verified` bit not null,
+    PRIMARY KEY (`student_id`),
+    unique `idx_openid` (`openid`),
+    unique `idx_mobile` (`mobile`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8 auto_increment = 1;
+--------------------------------------------------------------------------
+  DROP TABLE IF EXISTS `interview`;
+CREATE TABLE `interview` (
+    `interview_id` int(11) NOT NULL auto_increment,
+    `student_id` int(11),
+    `company` varchar(50) not null,
+    `address` varchar(200) not null,
+    `interview_time` datetime not null,
+    `create_time` datetime not null,
+    `status` tinyint not null,
+    `stars` tinyint,
+    `note` varchar(500),
+    `offer_url` varchar(300) not null,
+    PRIMARY KEY (`interview_id`),
+    index `idx_student_id` (`student_id`),
+    index `idx_interview_time` (`interview_time`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8 auto_increment = 1;
+--------------------------------------------------------------------------
+  DROP TABLE IF EXISTS `examination`;
+CREATE TABLE `examination` (
+    `exam_id` int(11) NOT NULL auto_increment,
+    `interview_id` int(11) not null,
+    `title` varchar(100),
+    `content` varchar(4000),
+    `likes` int(11) not null,
+    PRIMARY KEY (`exam_id`),
+    index `idx_interview_id` (`interview_id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8 auto_increment = 1;
+--------------------------------------------------------------------------
--- doc/DBSchema.md
+++ doc/DBSchema.md
@@ -8,7 +8,7 @@
 | avatar_url  | varchar(300)  |   | 头像url  |
 | status  | tinyint  |  非空，默认0 | 状态（0未激活，1激活）  |
 | gender  | tinyint  |   | 性别（0保密，1男，2女）  |
-| mobile  | varchar(20)  |  非空，唯一索引 | 手机号  |
+| mobile  | varchar(20)  |  唯一索引 | 手机号  |
 | email  | varchar(100)  |   | 邮箱  |
 | mobile_verified  | bit  |  非空，默认false | 手机验证  |
 | email_verified  | bit  | 非空，默认false  | 邮箱验证  |
@@ -22,7 +22,7 @@
 | address  | varchar(200)  | 非空 | 公司地址  |
 | interview_time  | datetime  | 非空 索引  | 面试时间  |
 | create_time  | datetime  |  非空 | 创建时间  |
-| status  | tinyint  | 非空  | 状态（待面试0，Offer1，等通知2，凉凉3，取消4）  |
+| status  | tinyint  | 非空 默认0  | 状态（待面试0，Offer1，等通知2，凉凉3，取消4）  |
 | stars  | tinyint  |   | 评星  |
 | note  | varchar(500)  |   | 备注  |
 | offer_url  | varchar(300)  |   | offer url  |
@@ -31,7 +31,7 @@
 | 字段  | 类型  | 约束  |  说明 |
 |---|---|---|---|
 | exam_id  | int(11)  | 主键 自增 | Id  |
-| interview_id | int(11)  | 外键 索引 | 面试Id  |
+| interview_id | int(11)  | 非空 外键 索引 | 面试Id  |
 | title  | varchar(100)  |  | 标题  |
 | content  | varchar(4000)  |  | OCR内容  |
 | likes  | int(11)  | 非空 默认0  | 点赞数  |
