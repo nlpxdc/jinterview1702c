2c761bb3e0709c7362f6fe129f8e8c63cd299c94
chenjiefei <nlpx_dc@hotmail.com>
2019-12-05 17:33:26 +0800

    init front

--- jinterviewsite/src/main/java/io/cjf/jinterviewsite/JinterviewsiteApplication.java
+++ jinterviewsite/src/main/java/io/cjf/jinterviewsite/JinterviewsiteApplication.java
@@ -1,9 +1,11 @@
 package io.cjf.jinterviewsite;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.cjf.jinterviewsite.dao")
 public class JinterviewsiteApplication {
 
     public static void main(String[] args) {
