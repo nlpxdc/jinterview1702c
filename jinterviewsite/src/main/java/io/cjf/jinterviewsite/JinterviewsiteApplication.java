a1f4d623dda935b93796608a185b7756fd3e4050
chenjiefei <nlpx_dc@hotmail.com>
2019-12-05 11:27:33 +0800

    first init site

---
+++ jinterviewsite/src/main/java/io/cjf/jinterviewsite/JinterviewsiteApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.jinterviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinterviewsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinterviewsiteApplication.class, args);
+    }
+
+}
---
+++ jinterviewsite/src/test/java/io/cjf/jinterviewsite/JinterviewsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.jinterviewsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinterviewsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
