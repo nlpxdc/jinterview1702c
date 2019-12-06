9820b2f9f0d1a811a24958edb71918abdf789e6e
chenjiefei <nlpx_dc@hotmail.com>
2019-12-06 20:48:07 +0800

    draft login page

--- jinterviewsite/src/main/java/io/cjf/jinterviewsite/controller/StudentController.java
+++ jinterviewsite/src/main/java/io/cjf/jinterviewsite/controller/StudentController.java
@@ -1,6 +1,8 @@
 package io.cjf.jinterviewsite.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/student")
 public class StudentController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/autoRegisterLogin")
     public Integer autoRegisterLogin(@RequestParam String code){
+        logger.info("code: {}", code);
         return null;
     }
 
