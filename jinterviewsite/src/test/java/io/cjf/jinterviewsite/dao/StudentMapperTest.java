ae40aca8e9d588f48e47b3f2b09570922881cfaa
chenjiefei <nlpx_dc@hotmail.com>
2019-12-07 14:55:25 +0800

    add student mapper insert test

--- jinterviewsite/src/test/java/io/cjf/jinterviewsite/dao/StudentMapperTest.java
+++ jinterviewsite/src/test/java/io/cjf/jinterviewsite/dao/StudentMapperTest.java
@@ -1,5 +1,6 @@
 package io.cjf.jinterviewsite.dao;
 
+import io.cjf.jinterviewsite.enumeration.StudentStatus;
 import io.cjf.jinterviewsite.po.Student;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +23,7 @@ class StudentMapperTest {
     @Autowired
     private StudentMapper studentMapper;
 
-    private String openid = "";
+    private String openid = UUID.randomUUID().toString();
 
     @BeforeEach
     void setUp() {
@@ -33,5 +37,22 @@ class StudentMapperTest {
     void selectByOpenid() {
         final Student student = studentMapper.selectByOpenid(openid);
         assertNotNull(student);
+    }
+
+    @Test
+//    @Rollback(false)
+    void insert(){
+        final Student student = new Student();
+        student.setOpenid(openid);
+        student.setNickname("mynickname");
+        student.setAvatarUrl("http://xxx.com/xxx.jpg");
+        student.setGender((byte)0);
+        student.setStatus((byte) StudentStatus.Disable.ordinal());
+        student.setMobileVerified(false);
+        student.setEmailVerified(false);
+
+        studentMapper.insert(student);
+        final Integer studentId = student.getStudentId();
+        assertNotNull(studentId);
     }
 }
