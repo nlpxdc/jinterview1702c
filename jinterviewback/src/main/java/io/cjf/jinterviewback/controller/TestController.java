c26d28d4013dee793f533b9bf08c11059aba283d
chenjiefei <nlpx_dc@hotmail.com>
2019-12-13 00:20:37 +0800

    fix download with token

--- jinterviewback/src/main/java/io/cjf/jinterviewback/controller/TestController.java
+++ jinterviewback/src/main/java/io/cjf/jinterviewback/controller/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RestController
