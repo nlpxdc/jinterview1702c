ad38989e4ac9c54f8d7f90063645b74c49159f84
珩git config --global user.email 1729860347@qq.comgit config --global user.name 珩 <1729860347@qq.com>
2019-12-13 09:46:28 +0800

    draft templatemessage serviceimpl

---
+++ jinterviewback/src/main/java/io/cjf/jinterviewback/service/impl/TemplateServiceImpl.java
@@ -0,0 +1,42 @@
+package io.cjf.jinterviewback.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import io.cjf.jinterviewback.service.TemplateService;
+import io.cjf.jinterviewback.template.TemplateMessage;
+
+public class TemplateServiceImpl implements TemplateService {
+
+    /**
+     * 模板消息请求路径
+     */
+    private static String  TemplateMessage_Url="https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN";
+
+    /**
+     *获取token路径
+     */
+    private static String AccessTokenUrl="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=SECRET";
+
+    private static String appId = "wx0c14a6dfeab19166";
+
+    private static String secret = "02a53bc213a98bb9fa6aae4157fd55eb";
+
+    @Override
+    public String TemplateMessage(String jsonString) {
+        //获取token
+        String token = AccessTokenUrl.replace("APPID",appId).replace("SECRET",secret);
+
+        TemplateMessage templateMessage = new TemplateMessage();
+
+        String url=TemplateMessage_Url.replace("ACCESS_TOKEN",token);
+
+        //调用接口发送
+        JSONObject jsonObject = httpRequest(url,"POST",jsonString);
+        Integer errcode = jsonObject.getInteger("errcode");
+        return null;
+    }
+
+    private JSONObject httpRequest(String url, String post, String jsonString) {
+
+        return null;
+    }
+}
