373b55848f2e2720247f286321576e0f4c67dd5c
珩git config --global user.email 1729860347@qq.comgit config --global user.name 珩 <1729860347@qq.com>
2019-12-13 15:18:20 +0800

    draft templatemessage

--- jinterviewback/src/main/java/io/cjf/jinterviewback/template/TemplateData.java
+++ jinterviewback/src/main/java/io/cjf/jinterviewback/template/TemplateData.java
@@ -2,49 +2,25 @@ package io.cjf.jinterviewback.template;
 
 public class TemplateData {
 
-    private  TemplateContent first;
-    private  TemplateContent keyword1;
-    private  TemplateContent keyword2;
-    private  TemplateContent keyword3;
-    private  TemplateContent remark;
+    //消息内容
+    private String value;
 
-    public TemplateContent getFirst() {
-        return first;
+    //消息字体颜色
+    private String color;
+
+    public String getValue() {
+        return value;
     }
 
-    public void setFirst(TemplateContent first) {
-        this.first = first;
+    public void setValue(String value) {
+        this.value = value;
     }
 
-    public TemplateContent getKeyword1() {
-        return keyword1;
+    public String getColor() {
+        return color;
     }
 
-    public void setKeyword1(TemplateContent keyword1) {
-        this.keyword1 = keyword1;
-    }
-
-    public TemplateContent getKeyword2() {
-        return keyword2;
-    }
-
-    public void setKeyword2(TemplateContent keyword2) {
-        this.keyword2 = keyword2;
-    }
-
-    public TemplateContent getKeyword3() {
-        return keyword3;
-    }
-
-    public void setKeyword3(TemplateContent keyword3) {
-        this.keyword3 = keyword3;
-    }
-
-    public TemplateContent getRemark() {
-        return remark;
-    }
-
-    public void setRemark(TemplateContent remark) {
-        this.remark = remark;
+    public void setColor(String color) {
+        this.color = color;
     }
 }
