632b9ed327c177766c5cd0e3e74239ef5abb0a54
贺向飞 <1132310433@qq.com>
2019-12-14 09:38:13 +0800

    add word pdf

--- jinterviewback/src/main/java/io/cjf/jinterviewback/controller/ChartController.java
+++ jinterviewback/src/main/java/io/cjf/jinterviewback/controller/ChartController.java
@@ -3,11 +3,19 @@ package io.cjf.jinterviewback.controller;
 import io.cjf.jinterviewback.po.Interview;
 import io.cjf.jinterviewback.service.InterviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.rtf.RtfWriter2;
+import org.apache.commons.codec.binary.Base64;
+import sun.misc.BASE64Decoder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -30,6 +38,114 @@ public class ChartController {
                 Collectors.groupingBy(Interview::getRealName, Collectors.counting()));
         return interCount;
     }
+
+
+
+
+    //保存到本地t图片
+    @PostMapping("/saveword")
+    public void pp(@RequestBody String img) throws IOException, DocumentException {
+        File f =new File("pdffile.jpg");
+
+        if(f.exists()){
+            f.delete();
+        }
+
+        String decode = URLDecoder.decode(img, "UTF-8");
+        String s = decode.replaceAll(" ", "+");
+        String substring1 = s.substring(22);
+        // Base64解码
+        Base64 base64 = new Base64();
+        BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+        byte[] b = decoder.decodeBuffer(substring1);
+
+        OutputStream out = new FileOutputStream(f);
+        out.write(b);
+        out.flush();
+        out.close();
+        saveword();;
+        savepdf();
+    }
+    //    保存本地word
+public void saveword() throws IOException, DocumentException {
+    File file = new File("report.doc");
+    if(file.exists()){
+        file.delete();
+    }
+    Document document = new Document(PageSize.A4);
+
+    RtfWriter2.getInstance(document, new FileOutputStream(file));
+    document.open();
+
+    Paragraph title = new Paragraph("学生面试数统计报告");
+    //
+    // 设置标题格式对齐方式
+
+    title.setAlignment(Element.ALIGN_CENTER);
+
+    // title.setFont(titleFont);
+
+    document.add(title);
+
+    com.lowagie.text.Image instance = com.lowagie.text.Image.getInstance("pdffile.jpg");
+    instance.setAbsolutePosition(0, 0);
+    instance.scalePercent(50);
+    document.add(instance);
+    document.close();
+
+}
+
+//    保存本地pdf
+
+    @GetMapping("/savepdf")
+
+    public void savepdf() throws IOException, DocumentException {
+        File file = new File("report.pdf");
+        if(file.exists()){
+            file.delete();
+        }
+        Document doc = new Document(PageSize.A4, 20, 20, 20, 20); //new一个pdf文档
+
+        PdfWriter.getInstance(doc, new FileOutputStream(file));
+        doc.open();//打开文档
+
+        BaseFont bfChinese = BaseFont.createFont(BaseFont.HELVETICA,
+                BaseFont.WINANSI, BaseFont.NOT_EMBEDDED);
+        doc.add(new Paragraph("sss 学生面试数统计报告"));
+        com.lowagie.text.Image instance = com.lowagie.text.Image.getInstance("pdffile.jpg");
+        instance.scalePercent(90);
+        doc.add(instance);
+        doc.close();
+
+    }
+
+    //下载word
+
+    @GetMapping(value = "/downword",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public byte[] downword() throws IOException {
+        File file = new File("report.doc");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] bytes = StreamUtils.copyToByteArray(fileInputStream);
+
+
+
+        return bytes;
+    }
+
+    //下载pdf
+
+    @GetMapping(value = "/downpdf",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public byte[] downpdf() throws IOException {
+        File file = new File("report.pdf");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] bytes = StreamUtils.copyToByteArray(fileInputStream);
+
+
+
+
+        return bytes;
+    }
+
 
 
 }
--- jinterviewfront/viewmodels/BarChart.js
+++ jinterviewfront/viewmodels/BarChart.js
@@ -1,72 +1,205 @@
 var app = new Vue({
     el: '#app',
     data: {
-        counts:{},
-        labels:[],
-        data:[],
+    
+     
+     
+        
+        counts: {},
+        labels: [],
+        data: [],
+        base: "",
         active:0,
     },
 
-    methods:{
-        // getStudentInfo(){
-        //     axios.get("http://localhost:8080/chart/student").then(res=>{
-        //         this.realname = res.data;
-        //         console.log(this.realname);
-        //         this.realname.forEach(item=>{
-        //             //console.log(item.realname)
-        //             this.labels.push(item.realname);
-        //         })
-        //     });
-        // },
-        onClickLeft(){
+    methods: {
+        onClickLeft() {
 
         },
-        onClickRight(){
+        onClickRight() {
 
         },
-        getInterviewCount(){
-            axios.get("/chart/interviewCount").then(res=>{
-                this.counts=res.data;
-            console.log(this.counts);
-            for(var key in this.counts){
-                this.data.push(this.counts[key]);
-                this.labels.push(key);
-            };
-            console.log(this.data);
-            console.log(this.labels);
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'bar',
-
-                // The data for our dataset
-                data: {
-                    labels:this.labels,
-                    datasets: [{
-                        label: '面试次数',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: this.data,
-                    }],
-                },
-
-                // Configuration options go here
-                options: {
-                    scales:{
-                        yAxes:[{
-                            ticks:{
-                                beginAtZero:true,//从0开始
-                                stepSize:2//刻度
+        getInterviewCount() {
+            axios.get("/chart/interviewCount").then(res => {
+                this.counts = res.data;
+                console.log(this.counts);
+                for (var key in this.counts) {
+                    this.data.push(this.counts[key]);
+                    this.labels.push(key);
+                };
+                console.log(this.data);
+                console.log(this.labels);
+                var myChart = echarts.init(document.getElementById('myChart'));
+                var option = {
+                    bezierCurve: false,
+                    animation: false,
+                    color: ['#3398DB'],
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                        }
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: [
+                        {
+                            type: 'category',
+                            data: this.labels,
+                            axisTick: {
+                                alignWithLabel: true
                             }
-                        }]
-                    }
-                }
-            });
-        });
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value'
+                        }
+                    ],
+                    series: [
+                        {
+                            name: '直接访问',
+                            type: 'bar',
+                            barWidth: '60%',
+                            data: this.data
+                        }
+                    ]
+                };
+                myChart.setOption(option);
+                var imgData = myChart.getDataURL();
+                this.base = imgData;
+                this.tj();
 
-        }
+
+                // var chart = new Chart(ctx, {
+                //     // The type of chart we want to create
+                //     type: 'bar',
+
+                //     // The data for our dataset
+                //     data: {
+                //         labels:this.labels,
+                //         datasets: [{
+                //             label: '面试次数',
+                //             backgroundColor: 'rgb(255, 99, 132)',
+                //             borderColor: 'rgb(255, 99, 132)',
+                //             data: this.data,
+                //         }],
+                //     },
+
+                //     // Configuration options go here
+                //     options: {
+                //         scales:{
+                //             yAxes:[{
+                //                 ticks:{
+                //                     beginAtZero:true,//从0开始
+                //                     stepSize:2//刻度
+                //                 }
+                //             }]
+                //         }
+                //     }
+
+
+
+                // });
+
+            });
+
+        },
+        tj() {
+            axios.post('/chart/saveword', this.base).
+                then(function (response) {
+                    console.log(response);
+                }).catch(function () {
+                    console.log('FAILURE!!');
+                });
+
+        },
+        pdf() {
+            axios(
+                {
+                    baseURL: "/jinterviewback",
+
+                    url: "/chart/downpdf",
+                    method: "get",
+                    responseType: 'blob',
+                    header: {
+                        'jinterviewToken': localStorage['jinterviewToken']
+                    }
+                })
+                .then(res => {
+
+                    console.log(res)
+                    const fileName = '面试次数图.pdf';
+
+
+                    console.log("response: ", res);
+                    // new Blob([data])用来创建URL的file对象或者blob对象
+                    let url = window.URL.createObjectURL(new Blob([res.data]));
+                    // 生成一个a标签
+                    let link = document.createElement("a");
+                    link.style.display = "none";
+                    link.href = url;
+                    // 生成时间戳
+                    let timestamp = new Date().getTime();
+                    link.download = timestamp + ".pdf";
+                    document.body.appendChild(link);
+                    link.click();
+
+
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        word() {
+            axios(
+                {
+                    baseURL: "/jinterviewback",
+                    url: "/chart/downword",
+                    method: "get",
+                    responseType: 'blob',
+                    header: {
+                        'jinterviewToken':localStorage['jinterviewToken']
+                    }
+                })
+                .then(res => {
+
+                    // console.log("response: ", res);
+                    // // new Blob([data])用来创建URL的file对象或者blob对象
+                    // let url = window.URL.createObjectURL(new Blob([res.data]));
+                    // // 生成一个a标签
+                    // let link = document.createElement("a");
+                    // link.style.display = "none";
+                    // link.href = url;
+                    // // 生成时间戳
+                    // let timestamp = new Date().getTime();
+                    // link.download = timestamp + ".doc";
+                    // document.body.appendChild(link);
+                    // link.click();
+
+
+                    let url = window.URL.createObjectURL(new Blob([res.data]));
+                    let link = document.createElement('a');
+                    link.style.display = 'none';
+                    link.href = url;
+                    link.setAttribute('download', '测试.doc');
+             
+                    document.body.appendChild(link);
+                    link.click()
+          
+
+
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
     },
-    mounted(){
+    mounted() {
         // this.getStudentInfo();
         this.getInterviewCount();
     }
