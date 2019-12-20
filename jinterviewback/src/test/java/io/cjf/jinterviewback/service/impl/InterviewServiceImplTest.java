992b6df4db76ef46c5ddf1d2b161aeb4a7929924
chenjiefei <nlpx_dc@hotmail.com>
2019-12-20 20:09:31 +0800

    add interview notify test

--- jinterviewback/src/test/java/io/cjf/jinterviewback/service/impl/InterviewServiceImplTest.java
+++ jinterviewback/src/test/java/io/cjf/jinterviewback/service/impl/InterviewServiceImplTest.java
@@ -1,11 +1,13 @@
 package io.cjf.jinterviewback.service.impl;
 
 import io.cjf.jinterviewback.service.InterviewService;
+import io.cjf.jinterviewback.vo.InterviewNotificationVO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +22,12 @@ class InterviewServiceImplTest {
         final Integer interviewId = interviewService.createInterview("永久", "南市区苏州河", new Date(), 1);
         assertNotNull(interviewId);
         assertTrue(interviewId != 0);
+    }
+
+    @Test
+    void getInterviewNotification() {
+        final Date now = new Date();
+        final List<InterviewNotificationVO> interviews = interviewService.getInterviewNotification(now);
+        assertTrue(interviews.size() != 0);
     }
 }
